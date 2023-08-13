eeae51d7c69d61e9fec066c3afb0a47595bd819d
Ira S <iryna.shevchenko2@gmail.com>
2023-08-13 04:06:37 +0300

    new variant

--- src/index.js
+++ src/index.js
@@ -4,99 +4,74 @@ import Notiflix from 'notiflix';
 
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const breedSelect = document.getElementById('breed-select');
-//   const catInfoDiv = document.querySelector('.cat-info');
-//   const loader = document.querySelector('.loader');
-
-//   const slimSelect = new SlimSelect({
-//     select: breedSelect,
-//     placeholder: 'Select a breed',
-//     showSearch: false,
-//     allowDeselect: true,
-//   });
-
-//   fetchBreeds()
-//     .then(data => {
-//       const breedOptions = data.map(item => ({ value: item.id, text: item.name }));
-//       slimSelect.setData(breedOptions);
-//     })
-//     .catch(error => {
-//       console.error('Error:', error);
-//       loader.classList.remove('loading');
-//     });
-  
-//   breedSelect.addEventListener('change', () => {
-//     const selectedOption = breedSelect.options[breedSelect.selectedIndex];
-//     const breedId = selectedOption.value;
-    
-//     fetchCatByBreed(breedId)
-//       .then(catData => {
-//         catInfoDiv.innerHTML = `
-//           <img src="${catData.url}" alt="Cat Image" width="400px" height="362px">
-//           <p><strong>Breed:</strong> ${catData.breeds[0].name}</p>
-//           <p><strong>Description:</strong> ${catData.breeds[0].description}</p>
-//           <p><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
-//         `;
-//       })
-//       .catch(error => {
-//         console.error('Error:', error);
-//       });
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
   const breedSelect = document.getElementById('breed-select');
   const catInfoDiv = document.querySelector('.cat-info');
   const loader = document.querySelector('.loader');
-   const errorElement = document.querySelector('.error');
+  const errorElement = document.querySelector('.error');
 
-  const slimSelect = new SlimSelect({
-    select: breedSelect,
-    placeholder: 'Select a breed',
-    showSearch: false,
-    allowDeselect: true,
-  });
+  // Function to toggle loader visibility
+  function toggleLoader(isLoading) {
+    if (isLoading) {
+      loader.style.display = 'inline-block';
+    } else {
+      loader.style.display = 'none';
+    }
+  }
 
+  // Fetch and populate breed options
+  toggleLoader(true);
   fetchBreeds()
     .then(data => {
-      const breedOptions = data.map(item => ({ value: item.id, text: item.name }));
-      breedOptions.unshift({ value: '', text: 'Select a breed', 'data-placeholder': 'true' }); // Add the placeholder option
-      slimSelect.setData(breedOptions);
+      data.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.text = breed.name;
+        breedSelect.appendChild(option);
+      });
 
-      loader.style.display = 'none';
+      // Hide select.breed-select and show p.loader
       breedSelect.style.display = 'block';
+      toggleLoader(false);
     })
     .catch(error => {
       console.error('Error:', error);
-      errorElement.style.display = 'block';
 
-      loader.style.display = 'none';
+      // Display p.error element and hide p.loader
+      errorElement.style.display = 'block';
+      toggleLoader(false);
     });
 
-  breedSelect.addEventListener('change', () => {
+  // Add event listener to the breedSelect element
+  breedSelect.addEventListener('change', async () => {
     const selectedOption = breedSelect.options[breedSelect.selectedIndex];
     const breedId = selectedOption.value;
 
-    loader.style.display = 'inline-block';
+    // Show div.cat-info and show p.loader
+    catInfoDiv.style.display = 'none';
+    toggleLoader(true);
 
-    fetchCatByBreed(breedId)
-      .then(catData => {
-        catInfoDiv.innerHTML = `
-          <img src="${catData.url}" alt="Cat Image" height="300" width="300">
-          <p><strong>Breed:</strong> ${catData.breeds[0].name}</p>
-          <p><strong>Description:</strong> ${catData.breeds[0].description}</p>
-          <p><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
-        `;
+    try {
+      const catData = await fetchCatByBreed(breedId);
 
-        errorElement.style.display = 'none';
-        loader.style.display = 'none';
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        errorElement.style.display = 'block';
+      // Update cat info in the div.cat-info block
+      catInfoDiv.innerHTML = `
+        <img src="${catData.url}" alt="Cat Image" height="300" width="300">
+        <p><strong>Breed:</strong> ${catData.breeds[0].name}</p>
+        <p><strong>Description:</strong> ${catData.breeds[0].description}</p>
+        <p><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
+      `;
 
-        loader.style.display = 'none';
-      });
+      // Hide div.cat-info and hide p.loader
+      catInfoDiv.style.display = 'block';
+      toggleLoader(false);
+      errorElement.style.display = 'none';
+    } catch (error) {
+      console.error('Error:', error);
+
+      // Display p.error element and hide p.loader
+      errorElement.style.display = 'block';
+      toggleLoader(false);
+    }
   });
 });
