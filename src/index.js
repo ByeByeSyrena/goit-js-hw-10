ec0a1e4b31f5174497baadb9edb3c901dcd80922
Ira S <iryna.shevchenko2@gmail.com>
2023-08-13 03:28:21 +0300

    error and empty placeholder

--- src/index.js
+++ src/index.js
@@ -49,17 +49,45 @@ document.addEventListener('DOMContentLoaded', () => {
   const breedSelect = document.getElementById('breed-select');
   const catInfoDiv = document.querySelector('.cat-info');
   const loader = document.querySelector('.loader');
+   const errorElement = document.querySelector('.error');
 
+  // Initialize the SlimSelect instance
+  const slimSelect = new SlimSelect({
+    select: breedSelect,
+    placeholder: 'Select a breed',
+    showSearch: false,
+    allowDeselect: true,
+  });
+
+  // Fetch and populate breed options
+  fetchBreeds()
+    .then(data => {
+      const breedOptions = data.map(item => ({ value: item.id, text: item.name }));
+      breedOptions.unshift({ value: '', text: 'Select a breed', 'data-placeholder': 'true' }); // Add the placeholder option
+      slimSelect.setData(breedOptions);
+
+      // Hide the loader after fetching breed data
+      loader.style.display = 'none';
+      breedSelect.style.display = 'block';
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      errorElement.style.display = 'block';
+
+      loader.style.display = 'none';
+    });
+
+  // Add event listener to the SlimSelect instance
   breedSelect.addEventListener('change', () => {
-    loader.style.display = 'block';
-    catInfoDiv.style.display = 'none';
-    breedSelect.style.display = 'none';
-
     const selectedOption = breedSelect.options[breedSelect.selectedIndex];
     const breedId = selectedOption.value;
 
+    // Show loader
+    loader.style.display = 'inline-block';
+
     fetchCatByBreed(breedId)
       .then(catData => {
+        // Update cat info in the div.cat-info block
         catInfoDiv.innerHTML = `
           <img src="${catData.url}" alt="Cat Image" height="300" width="300">
           <p><strong>Breed:</strong> ${catData.breeds[0].name}</p>
@@ -67,29 +95,14 @@ document.addEventListener('DOMContentLoaded', () => {
           <p><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
         `;
 
-    loader.style.display = 'none';
-    catInfoDiv.style.display = 'block';
-    breedSelect.style.display = 'block';
+        errorElement.style.display = 'none';
+        loader.style.display = 'none';
       })
       .catch(error => {
         console.error('Error:', error);
+        errorElement.style.display = 'block';
+
+        loader.style.display = 'none';
       });
   });
-
-  fetchBreeds()
-    .then(data => {
-      const breedOptions = data.map(item => ({ value: item.id, text: item.name }));
-      const slimSelect = new SlimSelect({
-        select: breedSelect,
-        placeholder: 'Select a breed',
-        showSearch: false,
-        allowDeselect: true,
-      });
-      loader.style.display = 'none';
-      breedSelect.style.display = 'block';
-      slimSelect.setData(breedOptions);    
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
 });
